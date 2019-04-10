27c468598585c9bcd0d848bb549731154cd5685a
1354107742 <1354107742@qq.com>
2019-04-10 10:23:44 +0800

    对runball函数进行修改

--- runball.py
+++ runball.py
@@ -1,8 +1,11 @@
+#获取测试用例
 n = int(input())
-
 for i in range (n):
+#利用切片将高度和弹跳次数进行统计
 	high,count = list(map(int,input().split()))
+	#保存初始高度
 	run = high
+	#进行第一次以后的高度运算
 	for j in range(count-1):
 			high = high / 2
 			run = run + high * 2
